d6adb29a7e288b7a31ef17b24fc5f5a294f59a35
LeoAlmeidaPereira <74634647+LeoAlmeidaPereira@users.noreply.github.com>
2020-11-17 22:55:02 -0300

    Update main.c

--- main.c
+++ main.c
@@ -8,16 +8,9 @@ int main(){
 	char harmoniaFinal[7], *ptr_Harmonia;
 	int semitom[3], *ptr_semitom, harmoniaInt[7], *ptr_harmoniaInt, i;
 	
-	/*printf("TOM ORIGINAL DA HARMONIA:\n");
-	scanf("%c", &tomOriginal);
-	printf("TOM IDEAL DA HARMONIA:\n");
-	fflush(stdin);
-	scanf("%c", &tomAtual);*/
-	
-	ptr_semitom = &semitom;//Guardar o tomOriginal, tomAtual e a dist‚ncia deles, tudo em Int
+	ptr_semitom = &semitom;//Guardar o tomOriginal, tomAtual e a dist√¢ncia deles, tudo em Int
 	ptr_Harmonia = &harmoniaFinal;
 	ptr_harmoniaInt = &harmoniaInt;
-	//char str_notas[12] = {'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'};
 
 	distanciaTonal(ptr_semitom, 'D', 'f');//Converter os tons de Char pra Int
 	
