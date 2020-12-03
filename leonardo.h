129ee280a25e3f2ea35f6ed21be995c47c1e7780
LeoAlmeidaPereira <74634647+LeoAlmeidaPereira@users.noreply.github.com>
2020-12-03 16:31:47 -0300

    Update leonardo.h

--- leonardo.h
+++ leonardo.h
@@ -1,7 +1,6 @@
 #ifndef FUNCOES_H_INCLUDED
 #define FUNCOES_H_INCLUDED
 
-
 void printArray(int *arr, int size);
 int nMaior(int n1, int n2); //Compara e retorna o maior numero
 int notaInt(char a); //Converte uma nota de char para int
@@ -9,5 +8,6 @@ char notaChar(int n); //Converte uma nota de int para char
 void distanciaTonal (int *ptr, char a, char b); //Retorna o tom atual e o tom desejado em int, alem de retornar a distancia de semitons entre elas
 void campoHarmonico (int *ptr);
 void criarHarmonicoInt(int tomFinal, int * harmoniaInt);
+void campoHarmonicoRec(int *ptr, int ctrl);
 
-#endif /* FUNCOES_H_INCLUDED */
+#endif
